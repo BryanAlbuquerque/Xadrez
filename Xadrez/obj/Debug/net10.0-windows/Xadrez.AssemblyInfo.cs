9e27aa880975aaf9082ec2f40fb5cf957ebd9f65
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-09-06 14:36:22 -0300

    Publish do Jogo

--- Xadrez/obj/Debug/net10.0-windows/Xadrez.AssemblyInfo.cs
+++ Xadrez/obj/Debug/net10.0-windows/Xadrez.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b7118cfbef894e4ac2233a62262bda3237a7a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72767737606d4081872e6306800f5d78a94a3ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
